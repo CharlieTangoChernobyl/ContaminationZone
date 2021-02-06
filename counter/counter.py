d2f22571bdab54c3d77a44934937a072a9828cbf
CharlieTangoChernobyl <69048410+CharlieTangoChernobyl@users.noreply.github.com>
2021-02-06 13:09:03 +0000

    Add files via upload

--- counter/counter.py
+++ counter/counter.py
@@ -11,6 +11,7 @@ from influxdb import InfluxDBClient
 counts = deque()
 hundredcount = 0
 usvh_ratio = 0.00812037037037 # This is for the J305 tube
+daily_ratio = 0.19488888888 # This is for the J305 tube
 
 # This method fires on edge detection (the pulse from the counter board)
 def countme(channel):
@@ -83,6 +84,41 @@ while True:
                 'fields': {
                     'cpm': int(len(counts)),
                     'usvh': "{:.2f}".format(len(counts)*usvh_ratio)
+                }
+            }
+        ]
+        
+        influx_client.write_points(measurements)
+        loop_count = 0
+    
+    # Update the displays with a zero-padded string
+    text_count = f"{len(counts):0>3}"
+    my_tube.set_digit(int(text_count[0]))
+    my_tube_m.set_digit(int(text_count[1]))
+    my_tube_r.set_digit(int(text_count[2]))
+    
+    time.sleep(1)
+    
+# In order to calculate daily dose we need to store a rolling count of events in the last 24 hours
+# This loop runs every second to update the Nixie display and removes elements from the queue
+# that are older than 60 seconds
+while True:
+    loop_count = loop_count + 1
+        
+    try:
+        while counts[0] < datetime.datetime.now() - datetime.timedelta(seconds=60):
+            counts.popleft()
+    except IndexError:
+        pass # there are no records in the queue.
+    
+    if loop_count == 10:
+        # Every 10th iteration (10 seconds), store a measurement in Influx
+        measurements = [
+            {
+                'measurement': 'balena-sense',
+                'fields': {
+                    'cpm': int(len(counts)),
+                    'dose': "{:.2f}".format(len(counts)*daily_ratio)
                 }
             }
         ]
