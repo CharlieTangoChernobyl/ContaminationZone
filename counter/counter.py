ce21e66d632ab394f04d1ee0b52a1c80a28b1a81
CharlieTangoChernobyl <69048410+CharlieTangoChernobyl@users.noreply.github.com>
2021-02-15 23:36:51 +0000

    Add files via upload

--- counter/counter.py
+++ counter/counter.py
@@ -5,7 +5,8 @@ import spidev
 import time
 import datetime
 import RPi.GPIO as GPIO
-import I2C_LCD_driver
+import smbus
+import time
 from time import *
 from collections import deque
 from influxdb import InfluxDBClient
@@ -13,7 +14,7 @@ from influxdb import InfluxDBClient
 counts = deque()
 hundredcount = 0
 usvh_ratio = 0.00812037037037 # This is for the J305 tube
-mylcd = I2C_LCD_driver.lcd()
+
 
 # This method fires on edge detection (the pulse from the counter board)
 def countme(channel):
@@ -28,7 +29,7 @@ def countme(channel):
         count100()
 
         
-mylcd.lcd_display_string("Hello World!", 1)
+
 
 # Set the input with falling edge detection for geiger counter pulses
 GPIO.setup(16, GPIO.IN)
@@ -66,3 +67,103 @@ while True:
         
         influx_client.write_points(measurements)
         loop_count = 0
+        
+# Define some device parameters
+I2C_ADDR  = 0x27 # I2C device address
+LCD_WIDTH = 20   # Maximum characters per line
+
+# Define some device constants
+LCD_CHR = 1 # Mode - Sending data
+LCD_CMD = 0 # Mode - Sending command
+
+LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
+LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
+LCD_LINE_3 = 0x94 # LCD RAM address for the 3rd line
+LCD_LINE_4 = 0xD4 # LCD RAM address for the 4th line
+
+LCD_BACKLIGHT  = 0x08  # On
+#LCD_BACKLIGHT = 0x00  # Off
+
+ENABLE = 0b00000100 # Enable bit
+
+# Timing constants
+E_PULSE = 0.0005
+E_DELAY = 0.0005
+
+#Open I2C interface
+#bus = smbus.SMBus(0)  # Rev 1 Pi uses 0
+bus = smbus.SMBus(1) # Rev 2 Pi uses 1
+
+def lcd_init():
+  # Initialise display
+  lcd_byte(0x33,LCD_CMD) # 110011 Initialise
+  lcd_byte(0x32,LCD_CMD) # 110010 Initialise
+  lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
+  lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off 
+  lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
+  lcd_byte(0x01,LCD_CMD) # 000001 Clear display
+  time.sleep(E_DELAY)
+
+def lcd_byte(bits, mode):
+  # Send byte to data pins
+  # bits = the data
+  # mode = 1 for data
+  #        0 for command
+
+  bits_high = mode | (bits & 0xF0) | LCD_BACKLIGHT
+  bits_low = mode | ((bits<<4) & 0xF0) | LCD_BACKLIGHT
+
+  # High bits
+  bus.write_byte(I2C_ADDR, bits_high)
+  lcd_toggle_enable(bits_high)
+
+  # Low bits
+  bus.write_byte(I2C_ADDR, bits_low)
+  lcd_toggle_enable(bits_low)
+
+def lcd_toggle_enable(bits):
+  # Toggle enable
+  time.sleep(E_DELAY)
+  bus.write_byte(I2C_ADDR, (bits | ENABLE))
+  time.sleep(E_PULSE)
+  bus.write_byte(I2C_ADDR,(bits & ~ENABLE))
+  time.sleep(E_DELAY)
+
+def lcd_string(message,line):
+  # Send string to display
+
+  message = message.ljust(LCD_WIDTH," ")
+
+  lcd_byte(line, LCD_CMD)
+
+  for i in range(LCD_WIDTH):
+    lcd_byte(ord(message[i]),LCD_CHR)
+
+def main():
+  # Main program block
+
+  # Initialise display
+  lcd_init()
+
+  while True:
+
+    # Send some test
+    lcd_string("RPiSpy         <",LCD_LINE_1)
+    lcd_string("I2C LCD        <",LCD_LINE_2)
+
+    time.sleep(3)
+  
+    # Send some more text
+    lcd_string(">         RPiSpy",LCD_LINE_1)
+    lcd_string(">        I2C LCD",LCD_LINE_2)
+
+    time.sleep(3)
+
+if __name__ == '__main__':
+
+  try:
+    main()
+  except KeyboardInterrupt:
+    pass
+  finally:
+    lcd_byte(0x01, LCD_CMD)
